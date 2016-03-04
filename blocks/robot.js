250956fba0687614ba0267ab3fc454171624ec4b
longmazhanfeng <1011909528@qq.com>
2016-03-04 16:20:34 +0800

    added 'tags' block and 'setup' block

--- blocks/robot.js
+++ blocks/robot.js
@@ -27,14 +27,20 @@
 goog.provide('Blockly.Blocks.robots');
 
 goog.require('Blockly.Blocks');
+goog.require('Blockly.Msg.rfui');
 
 
 /**
  * Common HSV hue for all blocks in this category.
  */
 Blockly.Blocks.texts.HUE = 160;
+// String frequently be used
+var SETUP_TITLE = '[Setup]';
+var TESTCASE_NAME = '用例名称：';
+var CASETITLE_TIPS = '设置用例名称';
+var TAGS = '[Tags]';
 
-Blockly.Blocks['case_title'] = {
+Blockly.Blocks['case_name'] = {
   /**
    * Block for testcase name
    * @this Blockly.Block
@@ -42,35 +48,31 @@ Blockly.Blocks['case_title'] = {
   init: function() {
     this.appendValueInput("name")
         .setCheck("String")
-        .appendField("用例名称：");
+        .appendField(TESTCASE_NAME);
     this.setInputsInline(true);
     this.setNextStatement(true, null);
     this.setColour(Blockly.Blocks.texts.HUE);
-    this.setTooltip('Set Testcase name');
+    this.setTooltip(CASETITLE_TIPS);
   }
 };
 
-Blockly.Blocks['case_settings'] = {
+Blockly.Blocks['setting_setup'] = {
   /**
-   * Block for testcase settings
-   * 
+   * Set testcase setup
+   * Block for creating a list with any number of elements of any type.
    * @this Blockly.Block
    */
   init: function() {
     this.setHelpUrl(Blockly.Msg.LISTS_CREATE_WITH_HELPURL);
-    this.setColour(Blockly.Blocks.texts.HUE);
+    this.setColour(Blockly.Blocks.lists.HUE);
+    this.setInputsInline(true);
+    this.itemCount_ = 1;
     this.updateShape_();
+    // this.setOutput(true, 'Array');
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    // this.setOutput(true, 'Array');
-    this.setMutator(new Blockly.Mutator(['setting_item', 'setting_casename', 'setting_tags', 'setting_setup']));
-    this.setTooltip("用例设置");
-
-    var thisBlock = this;
-    this.itemCount_ = 2;
-    this.casenameCount_ = 1;
-    this.tagsCount_ = 0;
-    this.setupCount_ = 0;
+    this.setMutator(new Blockly.Mutator(['lists_create_with_item']));
+    this.setTooltip(Blockly.Msg.LISTS_CREATE_WITH_TOOLTIP);
   },
   /**
    * Create XML to represent list inputs.
@@ -79,22 +81,7 @@ Blockly.Blocks['case_settings'] = {
    */
   mutationToDom: function() {
     var container = document.createElement('mutation');
-
-    if (this.itemCount_) {
-      container.setAttribute('items', this.itemCount_);
-    };
-
-    if (this.casenameCount_) {
-      container.setAttribute('casename', this.casenameCount_);
-    };
-
-    if (this.tagsCount_) {
-      container.setAttribute('tags', this.tagsCount_);
-    }
-    
-    if (this.setupCount_) {
-      container.setAttribute('setup', this.setupCount_);
-    }
+    container.setAttribute('items', this.itemCount_);
     return container;
   },
   /**
@@ -117,7 +104,7 @@ Blockly.Blocks['case_settings'] = {
     containerBlock.initSvg();
     var connection = containerBlock.getInput('STACK').connection;
     for (var i = 0; i < this.itemCount_; i++) {
-      var itemBlock = workspace.newBlock('setting_item');
+      var itemBlock = workspace.newBlock('lists_create_with_item');
       itemBlock.initSvg();
       connection.connect(itemBlock.previousConnection);
       connection = itemBlock.nextConnection;
@@ -182,101 +169,165 @@ Blockly.Blocks['case_settings'] = {
     // Rebuild block.
     if (this.itemCount_ == 0) {
       this.appendDummyInput('EMPTY')
-          .appendField(Blockly.Msg.LISTS_CREATE_EMPTY_TITLE);
+          .appendField(SETUP_TITLE);
     } else {
       for (var i = 0; i < this.itemCount_; i++) {
         var input = this.appendValueInput('ADD' + i);
         if (i == 0) {
-          input.appendField(Blockly.Msg.LISTS_CREATE_WITH_INPUT_WITH);
+          input.appendField(SETUP_TITLE);
         }
       }
     }
   }
 };
 
-Blockly.Blocks['setting_casename'] = {
-  /**
-   * Mutator bolck for testcase name.
-   * @this Blockly.Block
-   */
-  init: function() {
-    this.setColour(Blockly.Blocks.texts.HUE);
-    this.appendValueInput("test_name")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField("用例名称：");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setTooltip(Blockly.Msg.LISTS_CREATE_WITH_ITEM_TOOLTIP);
-    this.contextMenu = false;
-  }
-};
-
 Blockly.Blocks['setting_tags'] = {
   /**
-   * Mutator bolck for testcase tags.
+   * Set testcase setup
+   * Block for creating a list with any number of elements of any type.
    * @this Blockly.Block
    */
   init: function() {
-    this.setColour(Blockly.Blocks.texts.HUE);
-    this.appendValueInput("[Tags]")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField("[Tags]");
+    this.setHelpUrl(Blockly.Msg.LISTS_CREATE_WITH_HELPURL);
+    this.setColour(Blockly.Blocks.lists.HUE);
     this.setInputsInline(true);
+    this.itemCount_ = 1;
+    this.updateShape_();
+    // this.setOutput(true, 'Array');
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setTooltip(Blockly.Msg.LISTS_CREATE_WITH_ITEM_TOOLTIP);
-    this.contextMenu = false;
-  }
-};
-
-Blockly.Blocks['setting_setup'] = {
+    this.setMutator(new Blockly.Mutator(['lists_create_with_item']));
+    this.setTooltip(Blockly.Msg.LISTS_CREATE_WITH_TOOLTIP);
+  },
   /**
-   * Mutator bolck for testcase setup.
+   * Create XML to represent list inputs.
+   * @return {!Element} XML storage element.
    * @this Blockly.Block
    */
-  init: function() {
-    this.setColour(Blockly.Blocks.texts.HUE);
-    this.appendValueInput("[Setup]")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField("[Setup]");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setTooltip(Blockly.Msg.LISTS_CREATE_WITH_ITEM_TOOLTIP);
-    this.contextMenu = false;
-  }
-};
-
-Blockly.Blocks['setting_teardown'] = {
+  mutationToDom: function() {
+    var container = document.createElement('mutation');
+    container.setAttribute('items', this.itemCount_);
+    return container;
+  },
   /**
-   * Mutator bolck for testcase teardown.
+   * Parse XML to restore the list inputs.
+   * @param {!Element} xmlElement XML storage element.
+   * @this Blockly.Block
+   */
+  domToMutation: function(xmlElement) {
+    this.itemCount_ = parseInt(xmlElement.getAttribute('items'), 10);
+    this.updateShape_();
+  },
+  /**
+   * Populate the mutator's dialog with this block's components.
+   * @param {!Blockly.Workspace} workspace Mutator's workspace.
+   * @return {!Blockly.Block} Root block in mutator.
+   * @this Blockly.Block
+   */
+  decompose: function(workspace) {
+    var containerBlock = workspace.newBlock('lists_create_with_container');
+    containerBlock.initSvg();
+    var connection = containerBlock.getInput('STACK').connection;
+    for (var i = 0; i < this.itemCount_; i++) {
+      var itemBlock = workspace.newBlock('lists_create_with_item');
+      itemBlock.initSvg();
+      connection.connect(itemBlock.previousConnection);
+      connection = itemBlock.nextConnection;
+    }
+    return containerBlock;
+  },
+  /**
+   * Reconfigure this block based on the mutator dialog's components.
+   * @param {!Blockly.Block} containerBlock Root block in mutator.
+   * @this Blockly.Block
+   */
+  compose: function(containerBlock) {
+    var itemBlock = containerBlock.getInputTargetBlock('STACK');
+    // Count number of inputs.
+    var connections = [];
+    while (itemBlock) {
+      connections.push(itemBlock.valueConnection_);
+      itemBlock = itemBlock.nextConnection &&
+          itemBlock.nextConnection.targetBlock();
+    }
+    this.itemCount_ = connections.length;
+    this.updateShape_();
+    // Reconnect any child blocks.
+    for (var i = 0; i < this.itemCount_; i++) {
+      if (connections[i]) {
+        this.getInput('ADD' + i).connection.connect(connections[i]);
+      }
+    }
+  },
+  /**
+   * Store pointers to any connected child blocks.
+   * @param {!Blockly.Block} containerBlock Root block in mutator.
+   * @this Blockly.Block
+   */
+  saveConnections: function(containerBlock) {
+    var itemBlock = containerBlock.getInputTargetBlock('STACK');
+    var i = 0;
+    while (itemBlock) {
+      var input = this.getInput('ADD' + i);
+      itemBlock.valueConnection_ = input && input.connection.targetConnection;
+      i++;
+      itemBlock = itemBlock.nextConnection &&
+          itemBlock.nextConnection.targetBlock();
+    }
+  },
+  /**
+   * Modify this block to have the correct number of inputs.
+   * @private
+   * @this Blockly.Block
+   */
+  updateShape_: function() {
+    // Delete everything.
+    if (this.getInput('EMPTY')) {
+      this.removeInput('EMPTY');
+    } else {
+      var i = 0;
+      while (this.getInput('ADD' + i)) {
+        this.removeInput('ADD' + i);
+        i++;
+      }
+    }
+    // Rebuild block.
+    if (this.itemCount_ == 0) {
+      this.appendDummyInput('EMPTY')
+          .appendField(TAGS);
+    } else {
+      for (var i = 0; i < this.itemCount_; i++) {
+        var input = this.appendValueInput('ADD' + i);
+        if (i == 0) {
+          input.appendField(TAGS);
+        }
+      }
+    }
+  }
+};
+
+Blockly.Blocks['lists_create_with_container'] = {
+  /**
+   * Mutator block for list container.
    * @this Blockly.Block
    */
   init: function() {
-    this.setColour(Blockly.Blocks.texts.HUE);
-    this.appendValueInput("[Teardown]")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField("[Teardown]");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setTooltip(Blockly.Msg.LISTS_CREATE_WITH_ITEM_TOOLTIP);
+    this.setColour(Blockly.Blocks.lists.HUE);
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.LISTS_CREATE_WITH_CONTAINER_TITLE_ADD);
+    this.appendStatementInput('STACK');
+    this.setTooltip(Blockly.Msg.LISTS_CREATE_WITH_CONTAINER_TOOLTIP);
     this.contextMenu = false;
   }
 };
 
-Blockly.Blocks['setting_item'] = {
+Blockly.Blocks['lists_create_with_item'] = {
   /**
    * Mutator bolck for adding items.
    * @this Blockly.Block
    */
   init: function() {
-    this.setColour(Blockly.Blocks.texts.HUE);
+    this.setColour(Blockly.Blocks.lists.HUE);
     this.appendDummyInput()
         .appendField(Blockly.Msg.LISTS_CREATE_WITH_ITEM_TITLE);
     this.setPreviousStatement(true);
